13900c547becaaae116ef8d406662bc402e3b36d
JohnJosy <johnwewbfree@gmail.com>
2019-03-26 11:31:35 +0100

    creo le chiavi utili per l'autenticazione

---
+++ src/models/keys.js
@@ -0,0 +1,9 @@
+// Creo Un ogegto e lo esporto
+
+module.exports = {
+    instagram : {
+        clientId: 'ddb0841bfdc64553918e4d30f1433c02',
+        clientSecret: '148086488e7a45449f7a9e9cd80a099a',
+        accessToken: '8137087849.1677ed0.a665b0cd635b448893f9d5b6291e4c20'
+    }
+}
