61108b357854556fc784cdfa351427d478bf35a8
JohnJosy <johnwewbfree@gmail.com>
2019-03-26 16:04:12 +0100

    Aggiunto commento su path

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const morgan = require('morgan')
 const path = require('path')
 const Instagram = require('node-instagram').default;
 
-const indexRouter = require('./routes/index')
-const loginRouter = require('./routes/login')
+const indexRouter = require('./routes/index')//Richiamo il routing dell'index
+const loginRouter = require('./routes/login')//Richiamo il routing del login
 
 const app = express()
 
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000
 app.set('port', PORT)
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine','handlebars')
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'))//path.join unisce percorsi ; __dirname => da il percorso attuale
 
 // MIDDLEWARE
 app.use(morgan('dev'))//utilizzo la proprietà dev per vedere il log delle rooting
