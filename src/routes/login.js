bda7a99fafc3c43f2cf49b5158d0ca43e3c79e26
JohnJosy <johnwewbfree@gmail.com>
2019-03-26 14:46:11 +0100

    Add instanza del ModuloInstagram e gli passo i valori di key.js

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,6 +1,14 @@
 'use strict'
 
 const router = require('express').Router()
+const Instagram = require('node-instagram').default;
+const {clientId, clientSecret, accessToken} = require('../models/keys').instagram
+
+const instagram = new Instagram({
+    clientId: clientId,
+    clientSecret: clientSecret,
+    accessToken: accessToken
+})
 
 router.get('/', (req, res) => {
     res.render('login')
