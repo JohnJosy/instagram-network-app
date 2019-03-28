b9c73926348cc6bfe1aea02f74a9fd63a797ce71
JohnJosy <johnwewbfree@gmail.com>
2019-03-28 14:50:26 +0100

    passo a instagram Access Token e User Id

--- src/routes/handleauth.js
+++ src/routes/handleauth.js
@@ -14,8 +14,16 @@ router.get('/', async (req, res) => {
     try {
         const code = req.query.code
         const data = await instagram.authorizeUser(code, redirectUri)
+        req.session.accessToken = data.access_token //salvo l'Access token che ricevo da 'data' nella sessione del navigatore
+        req.session.userId = data.user.id //salvo l'user Id che ricevo da 'data' nella sessione del navigatore
+
+        instagram.config.accessToken = req.session.accessToken//passo a instagram accessToken e userId per poter far arrivare i dati da instagra
+        instagram.config.userId = req.session.userId//passo a instagram accessToken e userId per poter far arrivare i dati da instagra
+
+        console.log('dati per Autenticazione', instagram)
+
         console.log(data)
-        res.json(data)
+        res.json(instagram)
     } catch (error) {
         console.log(error)
         res.json(e)
