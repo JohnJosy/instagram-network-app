2295e4d525c8bb4187e75577afa291af36f81e15
JohnJosy <johnwewbfree@gmail.com>
2019-03-29 10:14:52 +0100

    Uso profileUserData.data e mediaPostsData.data

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -12,15 +12,11 @@ const instagram = new Instagram({
 router.get('/', async (req, res) => {
     try {
         const profileUserData = await instagram.get('users/self')//Predno i dati del profilo
-        console.log('ProfiloUtente', profileUserData)//Stampo dati profilo
+        console.log('/profile ProfiloUtente', profileUserData)//Stampo dati profilo
         const mediaPostsData = await instagram.get('users/self/media/recent')//Prendo i dati dei Post del profilo
-        console.log('DatiPostUtente: ', mediaPostsData )// Stampo dati Post
+        //console.log('/Profile DatiPostUtente: ', mediaPostsData )// Stampo dati Post
         res.render('profile',{
-            userName: profileUserData.data.username,
-            imgProfile: profileUserData.data.profile_picture,
-            fullName: profileUserData.data.full_name,
-            bioProfile: profileUserData.data.bio,
-            /* Ora Passo i dati dei Post ch eè un array */
+            user: profileUserData.data,
             posts: mediaPostsData.data
         })//renderizzo tutto a video creandio un oggetto con le proprietà prese dalle due richieste fatte prima
     } catch (error) {
